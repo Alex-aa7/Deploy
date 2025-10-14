bad8cdadc6a683d1b98eb1af4f32477b2778b739
Alexey Aleshin <aa7@inbox.ru>
2025-10-15 02:25:41 +0300

    screenshots added

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,52 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {globPatterns: ["**/*.{html,css,js,ico,png,svg}"]},
+      registerType: "autoUpdate",
+      workbox: { globPatterns: ["**/*.{html,css,js,ico,png,svg}"] },
       devOptions: {
-        enabled: true
+        enabled: true,
       },
       manifest: {
-        name: 'Alex App',
-        short_name: 'AlexApp',
-        description: 'A simple to-do application',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
+        name: "Alex App",
+        short_name: "AlexApp",
+        description: "A simple to-do application",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
         icons: [
           {
-            src: 'icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+            purpose: "any",
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        screenshots: [
+          {
+            src: "/screenshots/desktop.png",
+            type: "image/png",
+            sizes: "886x811",
+            form_factor: 'wide'
+          },
+            {
+            src: "/screenshots/mobile.png",
+            type: "image/png",
+            sizes: "886x811",
+            form_factor: 'narrow'
+          },
+        ],
+      },
+    }),
   ],
-  base: '/Deploy'
-})
+  base: "/Deploy",
+});
